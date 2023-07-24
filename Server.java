736f4682b378301dc5ec492ffc121dd55836fc05
Devendra Kanade <70775513+DevendraDK3@users.noreply.github.com>
2023-07-24 16:54:07 +0530

    Add files via upload

---
+++ Server.java
@@ -0,0 +1,34 @@
+import java.net.*;
+import java.io.*;
+
+public class Server {
+    public static void main(String Arg[]) throws Exception
+    {
+        System.out.println("Server Application is running");
+
+        ServerSocket ssobj = new ServerSocket(2100);
+        System.out.println("Server is running at port no : 2100");
+        
+  
+        Socket s = ssobj.accept();
+        System.out.println("Server and client connection is sucessful");
+
+        PrintStream ps = new PrintStream(s.getOutputStream());
+
+        BufferedReader br1 = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        BufferedReader br2 = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("****Chat Messagner Started****");
+        String str1,str2;
+        System.out.println("Enter message for Client");
+        while((str1 = br1.readLine())!=null)
+        {
+            System.out.println("Client says : "+ str1);
+            System.out.println("Enter message for client : ");
+            str2 = br2.readLine();
+            ps.println(str2);
+        }
+        System.out.println("Thank you for using Chat Messanger...");
+    }
+    
+}
